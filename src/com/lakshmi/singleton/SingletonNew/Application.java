966340e946da6cc7f3af9b3db5d5d2fd8afb03d3
lakshmi <mohandevthazhathethil@Mohans-MacBook-Pro.local>
2018-12-25 19:01:46 -0800

    singleton

--- src/com/lakshmi/singleton/SingletonNew/Application.java
+++ src/com/lakshmi/singleton/SingletonNew/Application.java
@@ -30,22 +30,78 @@ interface Screen
 public class Application {
 
 
-  /*  void drawCircle(Screen screen) {
+   public static String drawCircle(Screen screen)
+    {
         screen.setPixel(10, 11, "green");
+
+        System.out.println();
+        return "draw circle called";
     }
 
-    Screen s1  = LCDScreen.getInstance();
-    drawCircle(s);
-   Screen s2  = CRTScreen.getInstance();
-   drawCircle(s);
+
+    public static void main(String[] args) { // only i screen
+
+
+        Screen s1 = LCDScreen.getInstance();
+
+         String result1 = drawCircle(s1);
+          System.out.println(result1);
+
+        Screen s2 = CRTScreen.getInstance();
+        String result2 = drawCircle(s2);
+        System.out.println(result2);
+    }
 }
 
 
+
+
+
+
+
   class LCDScreen implements Screen{
+
+          private static LCDScreen soleInstance = null; //Lazy loading
+          private LCDScreen()
+          {
+          }
+
+
+          public static LCDScreen getInstance()
+          {
+              if(soleInstance == null)
+              {
+                  soleInstance = new LCDScreen();
+              }
+              return soleInstance;
+          }
+
     @Override
       public void setPixel(int x, int y, String color) {
 
       }
-  }*/
+  }
 
+class CRTScreen implements Screen{
+
+    private static CRTScreen soleInstance = null; //Lazy loading
+    private CRTScreen()
+    {
+    }
+
+
+    public static CRTScreen getInstance()
+    {
+        if(soleInstance == null)
+        {
+            soleInstance = new CRTScreen();
+        }
+        return soleInstance;
+    }
+
+    @Override
+    public void setPixel(int x, int y, String color) {
+
+    }
 }
+
